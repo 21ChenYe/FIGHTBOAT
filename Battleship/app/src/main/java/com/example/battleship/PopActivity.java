a4408a1c9fb3af20019e139b459882e3081bd7ce
Cullen Paulisick <clp0216@bu.edu>
2019-04-26 00:57:29 -0400

    Added Pop Up
    NOTE - activity labeled 'PopUp' is the test menu file for calling the pop up; the activity labeled 'PopActivity' with its corresponding xml file is the literal activity for the pop up
    - see comments in style.xml, drawable file pop_bg.xml, activity_pop.xml, and PopActivity.java for documentation

---
+++ Battleship/app/src/main/java/com/example/battleship/PopActivity.java
@@ -0,0 +1,41 @@
+package com.example.battleship;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.WindowManager;
+
+public class PopActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop);
+
+        //get display metrics for default screen
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        //set pop up dimensions
+        double width = (dm.widthPixels)*.6;
+        double height = (dm.heightPixels)*.5;
+
+        getWindow().setLayout((int)width, (int)height);
+
+        //adjust pop up position in screen
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x =  0;
+        params.y = -20;
+
+        getWindow().setAttributes(params);
+
+
+
+
+
+
+    }
+}
